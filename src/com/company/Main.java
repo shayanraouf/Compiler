704304647a9743627877a0cb71549fb8eeb754a1
shayan raouf <shayanraouf@gmail.com>
2017-03-28 20:36:28 -0700

    added skeleton and input steam reader

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args){
+        new Main().run(args[0]);
+    }
+
+    public static void run(String input){
+        try{
+            Scanner scanner = new Scanner(new File(input));
+            while(scanner.hasNext()){
+                System.out.println(scanner.next());
+            }
+            scanner.close();
+        }
+        catch (FileNotFoundException e){
+            System.err.println("File Not Found " + e);
+            System.exit(-1);
+        }
     }
 }
