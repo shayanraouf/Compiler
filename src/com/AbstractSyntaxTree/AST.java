0f38c62724164c3e0ba53e5c908135adcdd82f38
shayan raouf <shayanraouf@gmail.com>
2017-05-04 19:05:17 -0700

    well you know..

--- src/com/AbstractSyntaxTree/AST.java
+++ src/com/AbstractSyntaxTree/AST.java
@@ -8,6 +8,36 @@ public class AST {
     public AST(List<Line> lines) {
         this.lines = lines;
     }
+
+    public void display(){
+        for(Line line: lines){
+            display2(line.getStatement());
+            //System.out.println(line.getNumber());
+        }
+    }
+
+    public void display2(Statement statement){
+        while(statement != null){
+
+            if(statement instanceof Function){
+                Function func = (Function)statement;
+                displayFunction(func);
+            }
+
+            statement = statement.next;
+        }
+    }
+
+    public void displayFunction(Function func){
+        if(func == null) return;
+        System.out.println("(" + func.row + ", " + func.col +  ") function declaration");
+        System.out.println(func.functionName);
+        Statement statement = func.statements;
+        if(statement instanceof Function){
+            displayFunction((Function)statement);
+        }
+
+    }
 }
 
 
