608ae34857777cef887a28f496e359a34547e3d9
shayan raouf <shayanraouf@gmail.com>
2017-05-14 08:34:53 -0700

    added rest of AST functions

--- src/com/AbstractSyntaxTree/Expression.java
+++ src/com/AbstractSyntaxTree/Expression.java
@@ -1,7 +1,18 @@
 package com.AbstractSyntaxTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by shayanraouf on 4/14/2017.
  */
-public abstract class Expression extends Node{}
+public abstract class Expression extends Node{
+    List<Expression> expressions = new ArrayList<>();
+
+    public void addExpression(Expression e){
+        expressions.add(e);
+    }
+
+
+}
 
--- src/com/AbstractSyntaxTree/ForStatement.java
+++ src/com/AbstractSyntaxTree/ForStatement.java
@@ -8,7 +8,13 @@ public class ForStatement extends Statement {
     Expression          expr1;
     Expression          expr2;
     Expression          expr3;
-    Statement           statements;
+    BlockStatement      statements;
     String              exitLabel;
-    public ForStatement(){}
+
+    public ForStatement(Expression expr1, Expression expr2, Expression expr3, BlockStatement statements) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+        this.expr3 = expr3;
+        this.statements = statements;
+    }
 }
